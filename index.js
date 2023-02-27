513bbeeccc574e2087be8a4368212737740a54fd
fhossain <farhana.hossainse@gmail.com>
2023-02-27 12:22:54 -0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express ()
 const PORT = 8000
+const taskRoutes = require('./routes/taskRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 require("dotenv").config()
 require('./db/connection')
 
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.send('This home route is working')
-})
+app.use('/tasks', taskRoutes)
+app.use('/user', userRoutes)
 
 
 app.listen(PORT, () => {
