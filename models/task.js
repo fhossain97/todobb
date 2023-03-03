e4c25d1bc8323113c71bac23cb17b7200d4b987b
fhossain <farhana.hossainse@gmail.com>
2023-03-03 11:51:24 -0500

    updated models

--- models/task.js
+++ models/task.js
@@ -4,5 +4,13 @@ const taskSchema = new mongoose.Schema({
     title: String,
     description: String,
     date: Date,
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
     
 })
+
+const Task = mongoose.model('Task', taskSchema)
+
+module.exports = Task
